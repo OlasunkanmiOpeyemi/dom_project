fe0f4e9d2b4dfc8f964234934e06f534f450236f
Olasunkanmiopeyemi <olasunkanmiopeyemi1021@gmail.com>
2023-05-01 10:47:41 +0100

    domproject

---
+++ index.js
@@ -0,0 +1,56 @@
+const product = [
+  {
+    id: 0,
+    image: '.image/pixabay.jpg',
+    title: 'Z Flip Foldable Mobile',
+    price: 120,
+  },
+  {
+    id: 1,
+    image: '.image/pixabay.jpg',
+    title: 'Air Pods Pro',
+    price: 60,
+  },
+  {
+    id: 2,
+    image: '.image/pixabay.jpg',
+    title: '250D DSLR Camera',
+    price: 230,
+  },
+  {
+    id: 3,
+    image: '.image/pixabay.jpg',
+    title: 'Head Phones',
+    price: 100,
+  }
+];
+
+const categories = [...new set(product.map((item)=>{return item}))];
+
+let i=0;
+
+document.getElementById("root").innerHTML = categories.map((item)=>{
+  var {image, title, price} = item;
+  return (
+    `<div class = 'box'>
+      <div class='img-box'>
+        <img class='images' src= ${image}></img>
+      </div>
+      <div class = 'bottom'>
+        <p>${title}</p>
+        <h2>$ ${price}.00</h2>` +
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+      `</div>
+    </div>`
+  )
+}).join('');
+
+
+
+
+
+
+
+
+
+
